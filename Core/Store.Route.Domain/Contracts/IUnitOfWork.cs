4b72a755af926af0cf23a56e9799925988025df9
WissamTarik <www.wissamtarik2021@gmail.com>
2025-10-17 21:30:49 +0300

    APISession02

---
+++ Core/Store.Route.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Store.Route.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Route.Domain.Contracts
+{
+    public interface IUnitOfWork
+    {
+
+        IGenericRepository<TKey, TEntity> GetRepository<TKey, TEntity>() where TEntity:BaseEntity<TKey>;
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ Infrastructure/Store.Route.Persistance/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Store.Route.Domain.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Route.Persistance.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Name).HasColumnType("varchar")
+                                       .HasMaxLength(256);
+
+            builder.Property(p => p.Description)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(512);
+
+            builder.Property(p => p.PictureUrl).HasColumnType("varchar")
+                                              .HasMaxLength(256);
+
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+
+
+            builder.HasOne(p => p.Brand)
+                   .WithMany()
+                   .HasForeignKey(p => p.BrandId)
+                   .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(p => p.Type)
+                   .WithMany()
+                   .HasForeignKey(p => p.TypeId)
+                   .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
