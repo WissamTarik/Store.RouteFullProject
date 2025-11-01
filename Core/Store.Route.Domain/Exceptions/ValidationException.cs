8ff5f1d65eb03b538ec26fbe84b65febe4786c3e
WissamTarik <www.wissamtarik2021@gmail.com>
2025-11-01 10:58:30 +0200

    APISession06

---
+++ Core/Store.Route.Domain/Exceptions/ValidationException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Route.Domain.Exceptions
+{
+    public class ValidationException(IEnumerable<string> errors) : Exception("Validation Error")
+    {
+        public IEnumerable<string> Errors { get; } = errors;
+    }
+}
--- Core/Store.Route.Services/ServiceManager.cs
+++ Core/Store.Route.Services/ServiceManager.cs
@@ -1,10 +1,17 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using Store.Route.Domain.Contracts;
+using Store.Route.Domain.Entities.Identity;
 using Store.Route.Services.Abstractions;
+using Store.Route.Services.Abstractions.Auth;
 using Store.Route.Services.Abstractions.Basket;
 using Store.Route.Services.Abstractions.Products;
+using Store.Route.Services.Auth;
 using Store.Route.Services.Basket;
 using Store.Route.Services.Products;
+using Store.Route.Shared.JWT;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,12 +20,20 @@ using System.Threading.Tasks;
 
 namespace Store.Route.Services
 {
-    public class ServiceManager(IUnitOfWork _unitOfWork,ICacheRepository _cacheRepository,IMapper _mapper,IBasketRepository _basketRepository) : IServiceManager
+    public class ServiceManager(IUnitOfWork _unitOfWork,
+        ICacheRepository _cacheRepository,
+        IMapper _mapper,
+        IBasketRepository _basketRepository,
+        UserManager<AppUser> _userManager,
+        IOptions<JWTOptions> options
+        ) : IServiceManager
     {
         public IProductService ProductService { get; } = new ProductService(_unitOfWork,_mapper);
 
         public IBasketServices BasketServices { get; } = new BasketServices(_basketRepository,_mapper);
 
         public ICacheService CacheService { get; }=new CacheService(_cacheRepository);
+
+        public IAuthService AuthService { get; } = new AuthService(_userManager,options);
     }
 }
